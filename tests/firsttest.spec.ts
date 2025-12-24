e83afd1bfc46a13a5fea51cef92eb3a88a632e03
krishna <krishnamohankothapalli@outlook.com>
2025-12-24 16:42:12 -0500

    my first tests

---
+++ tests/firsttest.spec.ts
@@ -0,0 +1,21 @@
+import {test,expect} from '@playwright/test'
+
+test('To verify the home page', async ({page}) => {
+
+    //navigate to the web page
+    await page.goto('https://www.facebook.com/');
+
+    //to verify the page title
+    await expect(page).toHaveTitle(/Facebook/);
+});
+
+test('Input username and password', async({page}) => {
+    await page.goto('https://www.facebook.com/');
+
+    const userName = page.getByRole('textbox',{name: 'email'}); 
+    await userName.fill('kkothapalli0909@gmail.com');
+
+    const pBox = page.getByRole('textbox', {name: 'pass'});
+    await pBox.fill('testPassword');
+
+})
