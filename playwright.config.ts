8c7f0feecbafb85e25e887ae1246734abeaf0914
krishna <krishnamohankothapalli@outlook.com>
2025-12-29 16:05:37 -0500

    generated test with code gen, played around screenshot, video captures and retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,17 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
 
-    testMatch: ["login.test.ts"]
-
+    testMatch: ["recorded.test.ts"],
+    use: {
+        headless: false,
+        screenshot: "only-on-failure",
+        "video": "retain-on-failure",
+    },
+    retries: 2,
+    reporter: [["dot"], ["json", {
+        outputFile: "jsonReports/jsonReport.json"
+    }], ["html", {
+        open: "never"
+    }]]
 });
+  
